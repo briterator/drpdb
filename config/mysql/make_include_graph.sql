a2d27bd09cf209a2520efe233022dfab2211b28b
David Ziman <david.ziman@starbreeze.com>
2017-08-10 18:55:08 -0500

    Fix double quotes in stored procedures.

--- config/mysql/make_include_graph.sql
+++ config/mysql/make_include_graph.sql
@@ -7,8 +7,8 @@ CREATE PROCEDURE `make_include_graph`()
 	SQL SECURITY DEFINER
 	COMMENT ''
 BEGIN
-select compiland_symbol "Source", source_id "Target" from included;
-select src_files.path "Label",src_files.source_id "Id",  1 "type" from src_files
+select compiland_symbol 'Source', source_id 'Target' from included;
+select src_files.path 'Label',src_files.source_id 'Id',  1 'type' from src_files
 union all
-select symbols.name "Label", compilands.symbol "Id", 0 "type" from compilands inner join symbols on symbols.symbol=compilands.symbol;
+select symbols.name 'Label', compilands.symbol 'Id', 0 'type' from compilands inner join symbols on symbols.symbol=compilands.symbol;
 END
